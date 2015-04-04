d6a0e4eb8580c959b3d5a2e36ac72c99bfce7461
Tomás <t.duhourq@gmail.com>
2015-04-04 19:11:17 -0300

    Finish regex examples

--- src/RegexPractice/examples_ch11.py
+++ src/RegexPractice/examples_ch11.py
@@ -8,15 +8,24 @@ for line in choose_file():
     if re.search('From:', line):
         print line.rstrip()
 
-# ^  -> match the beginning of the line
-# .  -> match any character
-# *  -> many times
-# +  -> one or more times
-# \S -> match any non-whitespace character
-# [] -> lists a set of characters to match
+# ^   -> match the beginning of the line
+# .   -> match any character
+# *   -> many times
+# +   -> one or more times
+# \S  -> match any non-whitespace character
+# []  -> lists a set of characters to match
+# [^] -> match everything which is not inside this
+# ?   -> stop at first match, invalidates greedy match
+# ()  -> only extract the part inside the parenthesis
 for line in choose_file():
     if re.search('^From:', line):
         print line.rstrip()
 
-# Extract a list of matches given a regex
-print re.findall('[0-9]+', 'I am 22 years old')
+# Extract a list of strings matching a given regex
+print re.findall('[0-9]+', 'I am 22 years old and I like number 5')
+
+# Extract email
+print re.findall('\S+@\S+', 'My mail is t.duhourq@gmail.com')
+
+# Extract host
+print re.findall('@([^ ]+)', 'My mail is t.duhourq@gmail.com')
